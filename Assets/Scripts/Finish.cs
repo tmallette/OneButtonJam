4e509f4993fa961fe54d4ce2ed7c6bd971162dbd
Robert Junk <77463008+PanWhich@users.noreply.github.com>
2024-11-29 21:43:08 -0500

    Completed level 1 and added level 2

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -1,18 +1,23 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class Finish : MonoBehaviour
 {
+    public int NextLevel = 0;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log(collision.transform.tag);
-        if (collision.transform.CompareTag("Player")){
+        if (collision.transform.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(NextLevel);
             Debug.Log("You've Finished the level :)");
         }
     }
     private void Start() { }
 
-    private void Update() {
+    private void Update()
+    {
 
 
     }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -77,7 +77,7 @@ public class Player : MonoBehaviour
             JumpHoldEnd();
         }
 
-        if (rb.transform.position.y < -10f)
+        if (rb.transform.position.y < -25f || rb.transform.position.y < -20f || rb.transform.position.y > 180f)
         {
             AudioManager.Instance.PlaySFXClip(deadSound);
             Respawn();
